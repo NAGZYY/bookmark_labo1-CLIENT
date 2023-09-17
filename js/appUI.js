fd05a309bf72f684eb8bce49bbaf6328dc4208c4
NAGZY <68570686+NAGZYY@users.noreply.github.com>
2023-09-17 05:37:32 -0400

    test 34.1

--- js/appUI.js
+++ js/appUI.js
@@ -85,6 +85,11 @@ $(".category-item").on("click", function () {
             selectedCategories = uniqueCategories.slice(); // Sélectionnez toutes les catégories
             $(".category-item").addClass("selected"); // Cochez toutes les catégories
             $(".category-item .menuIcon").removeClass("fa-square").addClass("fa-check-square"); // Affichez l'icône fa-check-square pour toutes les catégories
+        } else {
+            // Si "Toutes les catégories" est déjà sélectionné, décochez-le
+            selectedCategories = [];
+            $(".category-item").removeClass("selected");
+            $(".category-item .menuIcon").removeClass("fa-check-square").addClass("fa-square");
         }
     } else {
         // Vérifiez si la catégorie est déjà sélectionnée
